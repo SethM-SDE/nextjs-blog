be79d509308e93a51626822c21aa6b0a01dd26ca
Seth <sethm.sde@gmail.com>
2022-04-22 18:09:39 -0400

    reformatted text to avoid escape characters, new scripts

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Lightning McQueen. I like to drive fast an turn left.</p>
+        <p>Hi, I am Lightning McQueen. I like to drive fast an turn left.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
